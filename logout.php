931569f146b3e16ae5c0d70645b4cf5ebca27b6c
erikza5 <83737632+erikza5@users.noreply.github.com>
2026-05-30 22:25:01 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-c:\xampp\htdocs\GATIFIN\login.php<?php
-session_start();      // Memulai sesi yang sedang aktif
-session_unset();      // Menghapus semua variabel sesi
-session_destroy();    // Menghancurkan sesi
-header("Location: login.php"); // Mengalihkan ke halaman login
-exit;
-?>
